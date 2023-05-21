5d59d5612a200df2216e2a15e4b26ba0ad68640e
amirulislam71 <amirultechon@gmail.com>
2023-05-21 16:41:40 +0600

    delete function create

--- src/Pages/MyToysPage/MyToys.jsx
+++ src/Pages/MyToysPage/MyToys.jsx
@@ -13,19 +13,31 @@ const MyToys = () => {
       });
   }, [user]);
 
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission logic here
+  };
+
+  const handleDelete = (id) => {
+    const proceed = confirm("are you want delete");
+    if (proceed) {
+      fetch(`http://localhost:5000/allToys/${id}`, {
+        method: "DELETE",
+      })
+        .then((res) => res.json)
+        .then((data) => {
+          console.log(data);
+        });
+    }
+  };
+
   return (
     <div className="bg-gradient-to-r from-black to-gray-400">
       <div className="container mx-auto pb-4">
         <div className="flex justify-between py-8 ">
           <p className="lg:text-4xl text-2xl font-mono font-extrabold text-red-600">
-            Available Car toys products
+            My toys products
           </p>
-
-          <select className="bg-orange-400 rounded-lg text-xl p-2">
-            <option>sorting</option>
-            <option>high price</option>
-            <option>low price</option>
-          </select>
         </div>
         <div className="overflow-x-auto w-full">
           <table className="table w-full">
@@ -70,10 +82,129 @@ const MyToys = () => {
                     <td>{toy.quantity}</td>
                     <td>{toy.rating}</td>
                     <td>
-                      <button className="btn btn-success  btn-xs">Edit</button>
+                      {/* The button to open modal */}
+                      <label
+                        htmlFor={`my-modal-${index}`}
+                        className="btn-success px-2 rounded-lg"
+                      >
+                        Edit
+                      </label>
+
+                      {/* The modal */}
+                      <input
+                        type="checkbox"
+                        id={`my-modal-${index}`}
+                        className="modal-toggle"
+                      />
+                      <div className="modal modal-bottom sm:modal-middle">
+                        <div className="modal-box">
+                          <div className="w-full mx-auto py-4">
+                            <form onSubmit={handleFormSubmit}>
+                              {/* Form fields */}
+                              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
+                                <div>
+                                  <div className="mb-4">
+                                    <label
+                                      htmlFor={`name-${index}`}
+                                      className="block text-sm font-medium"
+                                    >
+                                      Product Name
+                                    </label>
+                                    <input
+                                      type="text"
+                                      id={`name-${index}`}
+                                      className="mt-1 p-2 border rounded-md w-full"
+                                      required
+                                    />
+                                  </div>
+
+                                  <div className="mb-4">
+                                    <label
+                                      htmlFor={`subCategory-${index}`}
+                                      className="block text-sm font-medium"
+                                    >
+                                      Sub-category
+                                    </label>
+                                    <input
+                                      type="text"
+                                      id={`subCategory-${index}`}
+                                      className="mt-1 p-2 border rounded-md w-full"
+                                      required
+                                    />
+                                  </div>
+                                </div>
+                                <div>
+                                  <div className="mb-4">
+                                    <label
+                                      htmlFor={`price-${index}`}
+                                      className="block text-sm font-medium"
+                                    >
+                                      Price
+                                    </label>
+                                    <input
+                                      type="text"
+                                      id={`price-${index}`}
+                                      className="mt-1 p-2 border rounded-md w-full"
+                                      required
+                                    />
+                                  </div>
+
+                                  <div className="mb-4">
+                                    <label
+                                      htmlFor={`quantity-${index}`}
+                                      className="block text-sm font-medium"
+                                    >
+                                      Available Quantity
+                                    </label>
+                                    <input
+                                      type="number"
+                                      id={`quantity-${index}`}
+                                      className="mt-1 p-2 border rounded-md w-full"
+                                      required
+                                    />
+                                  </div>
+                                </div>
+                              </div>
+                              <div className="mb-4">
+                                <label
+                                  htmlFor={`description-${index}`}
+                                  className="block text-sm font-medium"
+                                >
+                                  Description
+                                </label>
+                                <textarea
+                                  id={`description-${index}`}
+                                  className="mt-1 p-2 border rounded-md w-full"
+                                  rows="4"
+                                  required
+                                ></textarea>
+                              </div>
+                              <div className="flex justify-end">
+                                <button
+                                  type="submit"
+                                  className="px-4 py-2 bg-blue-500 rounded-md hover:bg-blue-400 w-full mx-auto font-bold uppercase"
+                                >
+                                  Add Toy
+                                </button>
+                              </div>
+                            </form>
+                          </div>
+                          <div className="modal-action">
+                            <label
+                              htmlFor={`my-modal-${index}`}
+                              className="btn"
+                            >
+                              close
+                            </label>
+                          </div>
+                        </div>
+                      </div>
                     </td>
                     <td>
-                      <button className="btn btn-warning  btn-xs">
+                      <button
+                        onClick={() => handleDelete(category._id)}
+                        className="btn btn-warning btn-xs"
+                      >
                         Delete
                       </button>
                     </td>
